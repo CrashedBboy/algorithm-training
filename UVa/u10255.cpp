e8122f4348a8c58c9bfc202b05cac7860ad68d4e
CrashedBboy <crashedbboy@gmail.com>
2015-10-21 22:56:32 +0800

    uva 10255 done

--- UVa/u10255.cpp
+++ UVa/u10255.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
@@ -8,14 +9,14 @@ typedef struct {
 	int y;
 } Step;
 
-int size, startx, starty;
+int size, startx, starty, finalx, finaly;
 vector< vector<int> > board;
 vector<int> possibleStep;
 vector<int>::iterator it;
 int dir[8][2] = {{-2, -1}, {-2, 1}, {2, -1}, {2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}};
 
 Step step(int, int);
-void travel(Step);
+bool travel(Step);
 void possible(Step, vector<int>&);
 Step get(Step, int);
 Step hard(Step);
@@ -23,10 +24,17 @@ bool isVisitable(Step);
 void print_solution();
 
 int main(void) {
-	while (scanf("%d %d %d\n", &size, &startx, &starty) != EOF) {
+	while (scanf("%d %d %d", &size, &startx, &starty) != EOF) {
 		board.resize(size + 1);
+		for (int i = 1; i < board.size(); i++) {
+			board[i].assign(size + 1, 0);
+		}
 		possibleStep.clear();
-		travel(step(startx, starty));
+		if(travel(step(startx, starty)) && abs((finalx - startx) * (finaly - starty)) == 2){
+				print_solution();
+		} else {
+			printf("No Circuit Tour.\n");
+		}
 	}
 	return 0;
 }
@@ -38,23 +46,27 @@ Step step(int x, int y) {
 	return s;
 }
 
-void travel(Step start) {
+bool travel(Step start) {
 	board[start.x][start.y] = 1;
 	Step current = start;
 	for (int i = 2; i < size * size + 1; i++) {
 		possible(current, possibleStep);
 		int c = possibleStep.size();
 		if(c == 0)
-			break;
+			return false;
 		if(c == 1)
 			current = get(current, 1);
 		else
 			current = hard(current);
 		board[current.x][current.y] = i;
 	}
+	finalx = current.x;
+	finaly = current.y;
+	return true;
 }
 
 void possible(Step current, vector<int> &pos) {
+	pos.clear();
 	for(int i = 0; i < 8; i++) {
 		Step s = step(current.x + dir[i][0], current.y + dir[i][1]);
 		if (isVisitable(s)) {
@@ -64,8 +76,9 @@ void possible(Step current, vector<int> &pos) {
 }
 
 Step get(Step current, int number) {
-	vector<int>::iterator it = possibleStep.end();
-	return step(current.x + dir[*it][0], current.y + dir[*it][1]);
+	int i = possibleStep[number - 1];
+	Step s = step(current.x + dir[i][0], current.y + dir[i][1]);
+	return s;
 }
 
 Step hard(Step current) {
@@ -83,18 +96,18 @@ Step hard(Step current) {
 			minPossibleStep.assign(nextPossibleStep.begin(), nextPossibleStep.end());
 		}
 	}
-
 	return get(current, min + 1);
 }
 
 bool isVisitable(Step s) {
-	return !board[s.x][s.y];
+	return s.x <= size && s.x > 0 && s.y <= size && s.y > 0 && board[s.x][s.y] == 0;
 }
 
 void print_solution() {
-	for(int i = 0; i <= size; i++) {
-		for(int j = 0; j <= size; j++) {
-			
+	for(int i = 1; i <= size; i++) {
+		for(int j = 1; j <= size; j++) {
+			printf("%5d", board[i][j]);
 		}
+		printf("\n");
 	}
 }
