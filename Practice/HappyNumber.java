960a3c4d74352f69db5471fad8f50e24a70269c2
CrashedBboy <crashedbboy@gmail.com>
2016-01-12 12:35:48 +0800

    Algorithm practice for final exam

---
+++ Practice/HappyNumber.java
@@ -0,0 +1,39 @@
+import java.util.HashSet;
+
+public class HappyNumber {
+	public boolean isHappy(int n) {
+	    HashSet<Integer> set = new HashSet<Integer>();
+	 
+	    while(!set.contains(n)){
+	        set.add(n);
+	 
+	        n = sum(getDigits(n));
+	        if (n == 1)
+	            return true;
+	    }
+	 
+	    return false;
+	}
+	 
+	public int sum(int[] arr){
+	    int sum = 0;
+	    for(int i: arr){
+	        sum = sum + i*i;
+	    }
+	    return sum;
+	}
+	 
+	public int[] getDigits(int n){
+	    String s = String.valueOf(n);
+	    int[] result = new int[s.length()];
+	    int i=0;
+	 
+	    while(n>0){
+	        int m = n%10;
+	        result[i++] = m;
+	        n = n/10;
+	    }
+	 
+	    return result;
+	}
+}
