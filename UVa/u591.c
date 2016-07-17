3c590b0f79735b1c98941c7b6736c30ce3a8acfe
CrashedBboy <crashedbboy@gmail.com>
2016-07-17 23:31:27 +0800

    uva 591

---
+++ UVa/u591.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+	int h[50], round = 1;
+	while (1) {
+		int num, i, aver, ans = 0, sum = 0;
+		scanf("%d", &num);
+		if (num == 0) break;
+		for (i = 0; i < num; i++) {
+			scanf("%d", (h+i));
+			sum += h[i];
+		}
+		aver = sum / num;
+		for (i = 0; i < num; i++) {
+			int diff = aver - h[i];
+			if (diff > 0) {
+				ans += diff;
+			}
+		}
+		if (round >= 1) {
+			printf("\n");
+		}
+		printf("Set #%d\nThe minimum number of moves is %d.\n", round, ans);
+		round += 1;
+	}
+	return 0;
+}
