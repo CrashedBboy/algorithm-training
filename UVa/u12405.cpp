206cc44e38af5f427bfddd3b586491686aad8b6a
CrashedBboy <crashedbboy@gmail.com>
2016-07-20 16:49:15 +0800

    UVa 12405

---
+++ UVa/u12405.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	int round;
+	cin >> round;
+	for (int i = 0; i < round; i++) {
+		int len, ptr = 0, count = 0;
+		string field;
+		cin >> len >> field;
+		for (int j = 0; j < len; j++) {
+			if (j <= ptr) continue;
+			if (field[j] == '#') continue;
+			count += 1;
+			ptr = j + 2;
+		}
+		cout << "Case " << i+1 << ": " << count << endl;
+	}
+	return 0;
+}
